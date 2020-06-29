b70e1cddecbde0ee8ffa78de394d5d424fef6dc7
gracia10 <gracia10@daum.net>
2020-06-29 19:51:43 +0900

    [add] redis test (Lettuce)

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
+** https://docs.spring.io/spring-data/data-redis/docs/current/reference/html/
+
 :: redis 설치
 - 인메모리 솔루션
-
 - window 지원안함 . https://github.com/microsoftarchive/redis/releases/tag/win-3.2.100 에서 다운
+- 도커로 실행 https://hub.docker.com/_/redis/
 - 기본포트는 6379
 
 :: 지원 타입
 
-String 		Text, 숫자 등 일반적인 Value
-Set		Value가 Set 자료구조가 되므로, 하나의 Key 값에 여러 값을 Set 자료구조를 통해 저장 가능
-Sorted Set	score라는 필드가 추가되어 정렬의 기준이 됨.
-Hashes		Value가 Map 자료구조와 같은 Key/Value 형태가 됨
-List		Value가 linkedList 자료구조가 됨. Push/Pop 연산 가능. index 활용 질의 가능
+String 		Text, 숫자 등 일반적인 Value  
+Set		Value가 Set 자료구조가 되므로, 하나의 Key 값에 여러 값을 Set 자료구조를 통해 저장 가능  
+Sorted Set	score라는 필드가 추가되어 정렬의 기준이 됨  
+Hashes		Value가 Map 자료구조와 같은 Key/Value 형태가 됨  
+List		Value가 linkedList 자료구조가 됨. Push/Pop 연산 가능. index 활용 질의 가능  
 
+---
+:: java용 Redis Client library
+
+- Jedis     (상대적 가벼움)
+- Lettuce   (* SDR 2.0 부터 default. 비동기&반응스트림 지원)
 
 :: 스프링 제공
 
-1. StringRedisTemplate of RedisTemplate	- 문자 특화된 템플릿
+1. StringRedisTemplate of RedisTemplate	- 문자 특화된 템플릿. 별다른 설정 필요x
 2. CrudRepository
+
+:: RedisTemplate
+
--- src/test/java/com/example/redis/RedisApplicationTests.java
+++ src/test/java/com/example/redis/RedisApplicationTests.java
@@ -1,13 +1,78 @@
 package com.example.redis;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class RedisApplicationTests {
+public class RedisApplicationTests {
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
 
     @Test
-    void contextLoads() {
+    public void redisBasicFunction_guide1(){
+
+        //Given
+        String key = "key";
+        String value = "value";
+
+        // redis에 set
+        redisTemplate.opsForValue().set(key, value);
+
+        // redis에서 get
+        String result = (String) redisTemplate.opsForValue().get(key);      //value
+
+        // redis에서 data delete
+        redisTemplate.delete(key);
+
+        // redis에 해당 key를 가지고 있는지 확인
+        if (!redisTemplate.hasKey("999")) {
+            System.out.println("key 미존재");
+        }
+
+        System.out.println(result);
     }
 
+    @Test
+    public void redisBasicFunction_guide2(){
+
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        Employee employee = new Employee("myid","myname");
+
+        valueOperations.set("employee", employee);
+
+        Employee result = (Employee) valueOperations.get("employee");   // Employee [id=myid, name=myname, age=null]
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void redisBasicFunction_guide3(){
+
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+
+        Map<String, String> empBobMap = new HashMap<>();
+        empBobMap.put("name", "Bob");
+        empBobMap.put("age", "26");
+        empBobMap.put("id", "02");
+
+        String empBobKey = "emp:Bob";
+
+        hash.putAll(empBobKey, empBobMap);
+
+        Map<String,String> result = hash.entries(empBobKey);    // {name=Bob, id=02, age=26}
+
+        System.out.println(result);
+    }
 }
